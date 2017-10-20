f0f5847da954254f1de690d3b84bc74c11b02ca5
szeyan05 <szeyan.05@gmail.com>
2017-10-20 16:02:28 +0800

    remianing files to upload

---
+++ mainJavaScript.js
@@ -0,0 +1,20 @@
+//function for opening facebook in another tab
+function facebookLink() {
+    window.open("https://www.facebook.com/");
+}
+//function for opening twitter in another tab
+function twitterLink() {
+    window.open("https://twitter.com/?lang=en");
+}
+//function for opening instagram in another tab
+function instagramLink() {
+    window.open("https://www.instagram.com/");
+}
+//function for showing an alert window to inform user that the update was successful
+function successUpdate() {
+    alert("Update was sucessful!");
+}
+//function for showing an alert window to inform user that the review was succesfully submitted
+function reviewSuccess() {
+    alert("Review was successfully submitted!");
+}
